9c74a435e943e61dd820e058c5d7e26ba051c3f4
Alex <alex.real1850@gmail.com>
2025-03-07 15:56:11 +0200

    lesson_69

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-    const tabsParent = document.querySelector(".tabheader__items"),
-          tabs = tabsParent.querySelectorAll(".tabheader__item"),
-          tabsContent = document.querySelectorAll(".tabcontent");
+    const tabsParent = document.querySelector(".tabheader__items"),  //*Tabs
+        tabs = tabsParent.querySelectorAll(".tabheader__item"),
+        tabsContent = document.querySelectorAll(".tabcontent");
 
     const hideTabContent = () => {
         tabsContent.forEach(item => {
@@ -57,5 +57,79 @@ window.addEventListener("DOMContentLoaded", () => {
     //     setTimeout(changeTabContent, 150, tabsParent, tabsContent);
     // });
     //?
+
+    const deadline = "2025-03-10";  //*Timer
+
+    const getRemainingTime = (endTime) => {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endTime) - Date.parse(new Date());
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60 ),
+            seconds = Math.floor((t / 1000) % 60);
+        }
+
+        return {
+            total: t,
+            days,
+            hours,
+            minutes,
+            seconds,
+        };
+    };
+
+    const getZero = (num) => num >= 0 && num < 10 ? `0${num}` : num;
+
+    const setClock = (selector, endTime) => {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector("#days"),
+            hours = timer.querySelector("#hours"),
+            minutes = timer.querySelector("#minutes"),
+            seconds = timer.querySelector("#seconds"),
+            timeInterval = setInterval(updateClock, 1000);
+        
+        updateClock();
+
+        function updateClock() {
+            const t = getRemainingTime(endTime);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) clearInterval(timeInterval);
+        };
+    };
+
+    setClock(".timer", deadline);
+
+    //? My version
+    // const getTimerBlocks = () => document.querySelectorAll(".timer__block span");
+
+    // const setDeadlineTime = (ddDate) => new Date(ddDate) - new Date();
+
+    // const timerUpdate = (ddDate) => {
+    //     const calculateTimeLeft = () => {
+    //         const mlsLeft = setDeadlineTime(ddDate) / 86400000;
+    //         const days = Math.floor(mlsLeft),
+    //               hours = Math.floor((mlsLeft % 1) * 24),
+    //               minutes = Math.floor(((mlsLeft % 1) * 24 % 1) * 60),
+    //               seconds = Math.floor((((mlsLeft % 1) * 24 % 1) * 60 % 1) * 60);
+    //         return [days, hours, minutes, seconds];
+    //     };
+    //     setInterval(() => {
+    //         calculateTimeLeft().forEach((time, i) => {
+    //             getTimerBlocks()[i].innerText = time
+    //         });
+    //     }, 1000);
+    // };
+    // timerUpdate("March 20, 2025 00:00:00"); //2025-06-20T00:00:00
 
 });
